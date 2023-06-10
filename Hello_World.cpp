db04d39a6f04322d6a7daf77e2316d17160e54a7
HarshaKss <kssharsha771@gmail.com>
2023-06-10 13:54:31 +0530

    first commit

---
+++ Hello_World.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+bool linearSearch(int arr[], int n, int key) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            cout<<i+1<<endl;
+            return true; 
+        }
+    }
+    return false; 
+}
+
+int main() {
+    int arr[] = {3, 9, 2, 7, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int key;
+    cout << "Enter the key to search: ";
+    cin >> key;
+
+    if (linearSearch(arr, n, key)) {
+        cout << "Key found!" << endl;
+    } else {
+        cout << "Key not found!" << endl;
+    }
+
+    return 0;
+}
